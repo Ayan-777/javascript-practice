815426b11f41da0292c0a308e63880bec52b940a
Ayan Mondal <mondalayan01876@gmail.com>
2026-03-01 23:57:11 +0530

    Add message button and improve styling for counter application

--- level-5/problem-3/script.js
+++ level-5/problem-3/script.js
@@ -1,17 +1,30 @@
 let countel = document.getElementById("count");
 let plus = document.getElementById("plus");
 let minus = document.getElementById("minus");
+let message = document.getElementById("message");
 
 let count = 0;
 
 plus.addEventListener("click", () => {
-  count++;
-  countel.innerHTML = count;
+  if (count < 10) {
+    count++;
+    countel.innerHTML = count;
+  }
+  if (count === 10) {
+    message.innerHTML = "Max limit";
+  } else {
+    message.innerHTML = "";
+  }
 });
 
 minus.addEventListener("click", () => {
   if (count > 0) {
     count--;
     countel.innerHTML = count;
+  }
+  if (count === 0) {
+    message.innerHTML = "Min Limit";
+  } else {
+    message.innerHTML = "";
   }
 });

a158f9a1870a2c7d2e33777b0392263561d081ed
Ayan Mondal <mondalayan01876@gmail.com>
2026-03-20 18:00:21 +0530

    Enhance Todo App functionality with task count display and input validation

--- level-5/problem-28/script.js
+++ level-5/problem-28/script.js
@@ -1,32 +1,71 @@
-let input = document.getElementById('input');
-let submit = document.getElementById('addbtn');
-let list = document.getElementById('list');
+let input = document.getElementById("input");
+let submit = document.getElementById("addbtn");
+let list = document.getElementById("list");
+let message = document.getElementById("message");
 
-submit.addEventListener('click', () => {
-    let text = input.value;
+// load save tasks
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-    if(text === "") return; // prevent empty
-
-    let task = document.createElement('div');
-    task.classList.add('task');
-
-    let span = document.createElement('span');
-    span.innerHTML = text;
-
-    let delebtn = document.createElement('button');
-    delebtn.innerHTML = 'Delete';
-
-    delebtn.addEventListener('click', () => {
-        task.remove();
-    });
-
-    span.addEventListener('click', () => {
-        span.classList.toggle('line-through')
-    });
-
-    list.appendChild(task);
-    task.appendChild(span);
-    task.appendChild(delebtn);
-
-    input.value = '';
+// Render tasks on page loading
+tasks.forEach((text) => {
+  createTask(text);
 });
+
+updateCount();
+
+submit.addEventListener("click", () => {
+  let text = input.value;
+
+  if (text.trim() === "") {
+    message.innerHTML = "Please enter something";
+    return;
+  }
+
+  if (list.children.length >= 5) {
+    list.removeChild(list.firstChild);
+    tasks.shift();
+  }
+
+  tasks.push(text);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  createTask(text);
+  updateCount();
+
+  input.value = "";
+});
+
+function createTask(text) {
+  let task = document.createElement("div");
+  task.classList.add("task");
+
+  let span = document.createElement("span");
+
+  span.innerHTML = text;
+
+  let delebtn = document.createElement("button");
+  delebtn.innerHTML = "Delete";
+
+  delebtn.addEventListener("click", () => {
+    task.remove();
+
+    // remove from array
+    tasks = tasks.filter((x) => x !== text);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+
+    updateCount();
+  });
+
+  span.addEventListener("click", () => {
+    span.classList.toggle("line-through");
+  });
+
+  list.appendChild(task);
+  task.appendChild(span);
+  task.appendChild(delebtn);
+}
+
+function updateCount(){
+    message.innerHTML = `Total Tasks: ${tasks.length}`;
+}

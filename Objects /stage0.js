faba7b5bcad15fd21e9a0b5d1dc695589d8dfa81
Ayan Mondal <mondalayan01876@gmail.com>
2026-04-01 17:51:23 +0530

    stage 0 in object

---
+++ Objects /stage0.js
@@ -0,0 +1,56 @@
+// 🔥 Q1
+// Create an object for a book with:
+// title
+// author
+// price
+
+let book = {
+    title: 'I FEEL IN LOVE WITH HOPE',
+    author: 'LANCALI',
+    price: 339
+};
+
+// 🔥 Q2
+// Create an object for a student with:
+// name
+// age
+// isPassed
+
+let student ={
+    name: 'Oliver',
+    age: 17,
+    isPassed : true,
+};
+
+
+// 🔥 Q3
+// Create an object where values are:
+// number
+// string
+// array
+// another object
+
+let object = {
+    number: 555,
+    string: 'Hello',
+    array: [1,5,4,6],
+    obj:{
+        a: 'a',
+        b: 'b'
+    }
+};
+
+
+// 🔥 Q4
+// What will this return?
+// let obj = { a: 1, b: 2, a: 5 };
+// console.log(obj.a);
+// ░▒▓   …/my-all-coding-main/Objects    main ?   v22.22.0   17:40  
+// ❯ node stage0.js 
+// 5
+// i test it and it give us 5 the last one i think because sync
+
+
+// 🔥 Q5 (Thinking)
+// Why should we use objects instead of separate variables?
+// => suppose we have student list like around 50 student if we goging to save thire information in separate variables it goging hard like confuing so we do object like student1 and student 2 so on and inside student1 have this student all information and it's easy to access and understand

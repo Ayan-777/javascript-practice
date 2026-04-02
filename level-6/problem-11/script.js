fca6908a4484547f2d1d9e435668faa41aa701d6
Ayan Mondal <mondalayan01876@gmail.com>
2026-04-03 01:30:57 +0530

    Search & Highlight to Smart & Highlight

--- level-6/problem-11/script.js
+++ level-6/problem-11/script.js
@@ -2,26 +2,36 @@ let input = document.getElementById("input");
 let btn = document.getElementById("searchbtn");
 let result = document.getElementById("message");
 let word = document.getElementById("word");
+let casebtn = document.getElementById("caseEl");
 
 let originalText = word.textContent;
 
-btn.addEventListener("click", () => {
+function highlight() {
   let text = input.value.trim();
 
-  if(text === ''){
-    result.textContent = '';
+  if (text === "") {
+    word.innerHTML = originalText;
+    result.textContent = "";
     return;
   }
 
-  let regex = new RegExp(text, 'gi');
+  let flag = casebtn.checked ? "g" : "gi";
+  let regex = new RegExp(text, flag);
   let matches = originalText.match(regex);
-  let count = matches ? matches.length : '0 Not Found';
+  let count = matches ? matches.length : 0;
 
-  let highlight = originalText.replace(regex,(x) => {
-    return `<span style="background: yellow;">${x}</span>`
-  })
+  let highlightText = originalText.replace(regex, (x) => {
+    return `<span style="background: yellow;">${x}</span>`;
+  });
 
-  word.innerHTML = highlight;
-  result.textContent = `Matches: ${count}`;
+  word.innerHTML = highlightText;
 
-});
+  if (count === 0) {
+    result.textContent = "No results found";
+  } else {
+    result.textContent = `Matches: ${count}`;
+  }
+}
+
+btn.addEventListener("click", highlight);
+casebtn.addEventListener("change", highlight);

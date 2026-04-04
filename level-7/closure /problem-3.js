40ba0aa412d0f7e779838d000cb1eaeb383dd560
Ayan Mondal <mondalayan01876@gmail.com>
2026-04-04 23:33:23 +0530

    problem-3 done in closure

--- level-7/closure /problem-3.js
+++ level-7/closure /problem-3.js
@@ -14,12 +14,45 @@
 // ✔ decrement can go below (no limit)
 // ✔ reset → back to 0
 
+function createSmartCounter(limit) {
+  let count = 0;
+  let obj = {};
 
-function createSmartCounter(limit){
-    
+  obj.increment = function () {
+    if (count === limit) {
+      return "Limit reached";
+    } else {
+      count++;
+      return count;
+    }
+  };
+
+  obj.decrement = function () {
+    count--;
+    return count;
+  };
+
+  obj.getValue = function () {
+    return count;
+  };
+
+  obj.reset = function () {
+    count = 0;
+    return count;
+  };
+
+  return obj;
 }
 
+let counter = createSmartCounter(3);
 
+console.log(counter.increment()); // 1
+console.log(counter.increment()); // 2
+console.log(counter.increment()); // 3
+console.log(counter.increment()); // ❌ "Limit reached"
 
+console.log(counter.decrement()); // 2
+console.log(counter.getValue()); // 2
 
-
+counter.reset();
+console.log(counter.getValue());

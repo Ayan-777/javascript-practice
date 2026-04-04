6ff3ede9ec71b4f489b98c28900b79c646be6466
Ayan Mondal <mondalayan01876@gmail.com>
2026-04-04 23:15:59 +0530

    Add closure-based counter implementations for levels 7 Q1, Q2, and Q3

---
+++ level-7/closure /problem-3.js
@@ -0,0 +1,25 @@
+// 🚀 LEVEL 7 — Q3 (Advanced Counter: Limit + Reset)
+// 🎯 Goal
+// Upgrade your counter with real control features
+// 🧠 Problem
+// Create:
+// createSmartCounter(limit)
+// ❗ Rules
+// ❌ No global variables
+// ❌ Cannot access count directly
+// ✅ Must use closure
+// 💡 Behavior Rules
+// ✔ count starts at 0
+// ✔ increment stops at limit
+// ✔ decrement can go below (no limit)
+// ✔ reset → back to 0
+
+
+function createSmartCounter(limit){
+    
+}
+
+
+
+
+

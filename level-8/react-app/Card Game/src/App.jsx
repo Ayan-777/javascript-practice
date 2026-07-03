7780cdb4a8fd69a5e7dac4ffbf5249a9a8ba096d
Ayan-777 <mondalayan01876@gmail.com>
2026-07-04 01:18:05 +0530

    Update .gitignore and refactor App component imports

--- level-8/react-app/Card Game/src/App.jsx
+++ level-8/react-app/Card Game/src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
-// import MemoryCard from './MemoryGame'
+import MemoryCard from './MemoryGame'
 // import Experiment from './Experiment'
-import DataFetcher from './DataFetche'
+// import DataFetcher from './DataFetche'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-    <DataFetcher/>
-      {/* <h2>Hello</h2>
+    {/* <DataFetcher/> */}
+      <h2>Hello</h2>
       <MemoryCard />
-      <Experiment /> */}
+      {/* <Experiment /> */}
     </>
   )
 

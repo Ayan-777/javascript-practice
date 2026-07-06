7e6c1f01bca5222a959a12448de228694f960d51
Ayan-777 <mondalayan01876@gmail.com>
2026-07-07 01:42:15 +0530

    creating currency converter in react

---
+++ level-8/react-app/crypto-tracker/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import CryptoConverter from "./CryptoConverter";
+
+function App(){
+  return (
+    <>
+    <CryptoConverter/>
+    </>
+  )
+}
+
+export default App
---
+++ level-8/react-app/crypto-tracker/vite-project/src/CryptoConverter.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+
+function CryptoConverter() {
+  const [rates, setRates] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [usdAmount, setUsdAmount] = useState(0);
+
+  useEffect(() => {
+    async function startFetcing() {
+      try {
+        let res = await fetch(
+          `Enter your own Api key`,
+        );
+        if (!res.ok) throw new Error("Not Found!");
+        let data = await res.json();
+
+        setRates(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    startFetcing();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <h1>Crypto Converter App</h1>
+      <input
+        type="Number"
+        value={usdAmount}
+        onChange={(e) => setUsdAmount(Number(e.target.value))}
+      />
+      
+
+      <div>
+        <h3>Conversions from USD:</h3>
+        <p>🇦🇪 AED (UAE Dirham): {usdAmount * rates.conversion_rates.AED}</p>
+        <p>🇪🇺 EUR (Euro): {usdAmount * rates.conversion_rates.EUR}</p>
+        <p>🇮🇳 INR (Indian Rupee): {usdAmount * rates.conversion_rates.INR}</p>
+      </div>
+    </div>
+  );
+}
+
+export default CryptoConverter;
